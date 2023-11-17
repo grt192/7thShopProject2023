d203aa27cfdc6400139144be1b3cd7c9a699c17b
siyona101215@gmail.com <siyona101215@gmail.com>
2023-11-16 17:40:09 -0800

    added flywheel

---
+++ src/main/java/frc/robot/subsystems/flywheelSubsystems.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+//imports
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class flywheelSubsystems extends SubsystemBase {
+
+}
