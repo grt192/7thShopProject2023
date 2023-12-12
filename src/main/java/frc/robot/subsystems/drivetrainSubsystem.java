23d5c0f33ddfb885c21904d3aa4296ad6f9fd1fc
siyoyoCode <siyona101215@gmail.com>
2023-12-12 10:13:43 -0800

    edited dt code

--- src/main/java/frc/robot/subsystems/drivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/drivetrainSubsystem.java
@@ -51,7 +51,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
     
 
   }
-
+  //delete
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
