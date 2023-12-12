76e620c58ed24aa9838241b9999fa3d69e93cf8f
siyoyoCode <siyona101215@gmail.com>
2023-12-12 10:25:28 -0800

    edited solenoid code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,6 @@ import frc.robot.subsystems.FlywheelSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.PnuematicSubsystem;
 
